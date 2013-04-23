a7437ed7db633b5b69fc8e9ace6b681d6bf4b537
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2013-04-23 15:27:09 +1000

    Branching UI, remove sliders

--- ui.R
+++ ui.R
@@ -8,10 +8,18 @@ shinyUI(pageWithSidebar(
   
   sidebarPanel(
       h4("Input"),
-      wellPanel(numericInput("N", HTML("\\(N\\)"), min = 1, value = 1000, step = 1),
-                helpText("Sample size")),
+      wellPanel(
+        h6("Calculate:"),
+        radioButtons("my_method", "",
+                       list("Power" = "power",
+                            "Sample size" = "samp_size")),
+        h6("Provide:"), 
+      conditionalPanel(condition = 'input.my_method == "power"',
+                       numericInput("N", "Sample size", min = 1, value = 1000, step = 1)),
+      conditionalPanel(condition = 'input.my_method == "samp_size"',
+                       numericInput("epower", "Power", min = 0, max = 1, value = 0.8))),
 
-      wellPanel(sliderInput("alpha", HTML("\\(\\alpha\\)"), min = 0, max = 1, value = 0.05),
+      wellPanel(numericInput("alpha", HTML("\\(\\alpha\\)"), min = 0, max = 1, value = 0.05),
                 helpText("Type-I error rate")),
       
       wellPanel(numericInput("byx", HTML("\\(\\beta_{yx}\\)"), value = 0),
@@ -20,15 +28,12 @@ shinyUI(pageWithSidebar(
       wellPanel(numericInput("bOLS", HTML("\\(\\beta_{OLS}\\)"), value = 0),
                 helpText("The regression \\(\\beta_{OLS}\\) coefficient for the observational association between the exposure \\((X)\\) and outcome \\((Y)\\) variables")),
       
-      wellPanel(sliderInput("R2xz", HTML("\\(R^2_{xz}\\)"), min = 0, max = 1, value = 0.01),
+      wellPanel(numericInput("R2xz", HTML("\\(R^2_{xz}\\)"), min = 0, max = 1, value = 0.01),
                 helpText("Proportion of variance explained for the association between the snp \\((Z)\\) and the exposure variable \\((X)\\)")),
       
       wellPanel(numericInput("varx", HTML("\\(\\sigma^2(x)\\)"), value = 1)),
       
-      wellPanel(numericInput("vary", HTML("\\(\\sigma^2(y)\\)"), value = 1)),
-      
-      wellPanel( sliderInput("epower", "Power:", min = 0, max = 1, value = 0.8))
-      ),
+      wellPanel(numericInput("vary", HTML("\\(\\sigma^2(y)\\)"), value = 1))),
   
   mainPanel(
         wellPanel(
