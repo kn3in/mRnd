59af230e8edc7bb2b4ed0c5c915ae5814be8d917
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2013-04-29 15:45:28 +1000

    Error handling for negative vey

--- functions.R
+++ functions.R
@@ -6,23 +6,30 @@ results <- function(N, alpha, byx, bOLS, R2xz, varx, vary, epower) {
     con <- (bOLS - byx) * varx # covariance due to YX confounding
     vey <- vary - byx * varx * (2 * bOLS - byx)
     
-    if (is.na(epower)) {
-        b2sls <- byx + con / (N * R2xz)
-        v2sls <- vey / (N * R2xz * varx)
-        NCP <- b2sls^2 / v2sls
-        # 2-sided test
-        power <- 1 - pchisq(threschi, 1, NCP)
-        data.frame(Parameter = c("Power", "NCP", "F-statistics"), Value = c(power, NCP, f.value), Description = c("", "Non-Centrality-Parameter", "The strength of the instrument"))    
+    if (vey < 0) {
+    
+        data.frame(Error = "Error: Reconsder input, provided parameters resulted in negative estimates for variance of error term in second step of two step MR model.")
+    
     } else {
-        # Calculation of sample size given power
-        z1 <- qnorm(1 - alpha / 2)
-        z2 <- qnorm(epower)
-        Z  <- (z1 + z2)^2
-        # Solve quadratic equation in N
-        a <- (byx * R2xz)^2
-        b <- R2xz * (2 * byx * con - Z * vey / varx)
-        c <- con^2
-        N1 <- (-b + sqrt(b^2 - 4 * a * c)) / (2 * a)
-        data.frame(Parameter = "Sample Size", Value = N1)
+
+        if (is.na(epower)) {
+            b2sls <- byx + con / (N * R2xz)
+            v2sls <- vey / (N * R2xz * varx)
+            NCP <- b2sls^2 / v2sls
+            # 2-sided test
+            power <- 1 - pchisq(threschi, 1, NCP)
+            data.frame(Parameter = c("Power", "NCP", "F-statistics"), Value = c(power, NCP, f.value), Description = c("", "Non-Centrality-Parameter", "The strength of the instrument"))    
+        } else {
+            # Calculation of sample size given power
+            z1 <- qnorm(1 - alpha / 2)
+            z2 <- qnorm(epower)
+            Z  <- (z1 + z2)^2
+            # Solve quadratic equation in N
+            a <- (byx * R2xz)^2
+            b <- R2xz * (2 * byx * con - Z * vey / varx)
+            c <- con^2
+            N1 <- (-b + sqrt(b^2 - 4 * a * c)) / (2 * a)
+            data.frame(Parameter = "Sample Size", Value = N1)
+        }
     }
 }
