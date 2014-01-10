0c50492f9a7b976f198b315f0c5d4734c83f25f8
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2014-01-10 12:43:38 +1000

    canvas for tab based app

--- ui.R
+++ ui.R
@@ -8,8 +8,11 @@ shinyUI(
     customHeaderPanel("mRnd: Power calculations for Mendelian Randomization"),
     
     sidebarPanel(
-      conditionalPanel(condition = "input.conditionedPanels==1", app_input())),
+      conditionalPanel(condition = "input.conditionedPanels==1", app_input_cont()),
+      conditionalPanel(condition = "input.conditionedPanels==2", app_input_binary())),
     
     mainPanel(
-      tabsetPanel(tabPanel("Continuous outcome", value = 1, app_output()) ,id = "conditionedPanels")
+      tabsetPanel(tabPanel("Continuous outcome", value = 1, app_output_cont()),
+                  tabPanel("Binary outcome", value = 2, app_output_binary()),
+                  id = "conditionedPanels")
 )))
