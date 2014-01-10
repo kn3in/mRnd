a03f1ee979e9527fbfaca38508019ff5c54a3f2f
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2014-01-10 13:31:53 +1000

    added function for binary outcome

--- functions.R
+++ functions.R
@@ -60,6 +60,37 @@ results_beta_based <- function(N, alpha, R2xz, varx, vary, byx, epower) {
     }
 }
 
+# for now sole responsibility for this function on others ;)
+# have not tested it or verified derivation
+
 results_binary <- function(N, alpha, R2xz, K, OR, epower) {
-   data.frame(x = "I don't do anything at the moment")
+    threschi <- qchisq(1 - alpha, 1) # threshold chi(1) scale
+    f.value <- 1 + N * R2xz / (1 - R2xz)
+
+    if (is.na(epower)) {
+
+        b_MR <- K * ( OR/ (1 + K * (OR - 1)) -1)
+        
+        v_MR <- (K * (1-K) - b_MR^2) / (N*R2xz)
+        NCP <- b_MR^2 / v_MR
+        
+        # 2-sided test
+        power <- 1 - pchisq(threschi, 1, NCP)
+        data.frame(Parameter = c("Power", "NCP", "F-statistic"), Value = c(power, NCP, f.value), Description = c("", "Non-Centrality-Parameter", "The strength of the instrument"))    
+    
+
+    } else {
+    
+        # Calculation of sample size given power
+        z1 <- qnorm(1 - alpha / 2)
+        z2 <- qnorm(epower)
+        Z  <- (z1 + z2)^2
+
+        b_01 <- K * ( OR/ (1 + K * (OR - 1)) -1)
+        f <- K * (1-K) - b_01^2
+        N1 <- Z * f / (b_01^2 * R2xz)
+        N1 <- ceiling(N1)
+        data.frame(Parameter = "Sample Size", Value = N1)
+    
+    }
 }
