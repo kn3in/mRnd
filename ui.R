21e937cce209e994d0dd9778611c542b1e3e0193
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2014-01-10 13:01:42 +1000

    move citation in a separate function, call it in a separate tab

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 library(shiny)
 source("custom_html.R")
-source("cont_input.R")
-source("cont_output.R")
+source("app_inputs.R")
+source("app_outputs.R")
 
 shinyUI(
   pageWithSidebar(
@@ -9,10 +9,12 @@ shinyUI(
     
     sidebarPanel(
       conditionalPanel(condition = "input.conditionedPanels==1", app_input_cont()),
-      conditionalPanel(condition = "input.conditionedPanels==2", app_input_binary())),
+      conditionalPanel(condition = "input.conditionedPanels==2", app_input_binary()),
+      conditionalPanel(condition = "input.conditionedPanels==3", list())),
     
     mainPanel(
       tabsetPanel(tabPanel("Continuous outcome", value = 1, app_output_cont()),
-                  tabPanel("Binary outcome", value = 2, app_output_binary()),
+                  tabPanel("Binary outcome",     value = 2, app_output_binary()),
+                  tabPanel("Citation",           value = 3, app_about()),
                   id = "conditionedPanels")
 )))
