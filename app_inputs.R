01cb4d39d0b231332408870c1359d1600f96f91c
Konstantin Shakhbazov <Konstantin.Shakhbazov@gmail.com>
2014-01-10 13:03:41 +1000

    binary input ui

--- app_inputs.R
+++ app_inputs.R
@@ -32,5 +32,27 @@ app_input_cont <- function() {
 }
 
 app_input_binary <- function() {
- list()
+ list(h4("Input"),
+      wellPanel(
+        h6("Calculate:"),
+        radioButtons("my_method2", "",
+                       list("Power" = "power2",
+                            "Sample size" = "samp_size2")),
+        h6("Provide:"), 
+      conditionalPanel(condition = 'input.my_method2 == "power2"',
+                       numericInput("N2", "Sample size", min = 1, value = 1000, step = 1)),
+      conditionalPanel(condition = 'input.my_method2 == "samp_size2"',
+                       numericInput("epower2", "Power", min = 0, max = 1, value = 0.8))),
+
+      wellPanel(numericInput("alpha2", HTML("\\(\\alpha\\)"), min = 0, max = 1, value = 0.05),
+                helpText("Type-I error rate")),
+      
+      wellPanel(numericInput("K", HTML("\\(K\\)"), value = 0),
+                helpText("Proportion of cases in the study")),
+      
+      wellPanel(numericInput("OR", HTML("\\(OR\\)"), value = 0),
+                helpText("True odds ratio of the outcome variable per standard deviation of the exposure variable")),
+      
+      wellPanel(numericInput("R2xz2", HTML("\\(R^2_{xz}\\)"), min = 0, max = 1, value = 0.01),
+                helpText("Proportion of variance explained for the association between the snp \\((Z)\\) and the exposure variable \\((X)\\)")))
 }
