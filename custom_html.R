9b6da7a0150c3da2036cab1a60d1ae99e5a0c19f
Konstantin Shakhbazov <konstantin.shakhbazov@gmail.com>
2014-09-05 17:40:44 +0200

    proper path to mathjax

--- custom_html.R
+++ custom_html.R
@@ -2,7 +2,7 @@ customHeaderPanel <- function(title, windowTitle = title) {
     tagList(
        tags$head(
           tags$title(windowTitle),
-          tags$script(src="https://c328740.ssl.cf1.rackcdn.com/mathjax/2.0-latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"),
+          tags$script(src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"),
           tags$script(src="js/ga.js")
        ),
        div(class = "span12", style = "padding: 10px 0px;", h1(title))
