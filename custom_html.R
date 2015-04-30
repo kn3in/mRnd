f727ab1c083e76db877f6c1bfbbe0d1e68986fa0
Konstantin Shakhbazov <konstantin.shakhbazov@gmail.com>
2015-05-01 09:20:34 +1000

    drop http from link to mathjax, shinyappio servers as http _s_

--- custom_html.R
+++ custom_html.R
@@ -2,7 +2,7 @@ customHeaderPanel <- function(title, windowTitle = title) {
     tagList(
        tags$head(
           tags$title(windowTitle),
-          tags$script(src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"),
+          tags$script(src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"),
           tags$script(src="js/ga.js")
        ),
        div(class = "span12", style = "padding: 10px 0px;", h1(title))
